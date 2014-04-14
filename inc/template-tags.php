277aff61bcc47da3b4de2f4d8699f67f85426a17
Justin Kopepasah <justin@kopepasah.com>
2014-04-14 14:20:01 +0900

    Change the way the time difference function outputs the time stamp.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -139,18 +139,50 @@ function eighties_get_time_difference( $time ) {
 	$difference = $current_time->diff( $previous_time );
 	$timestamp = '';
 
-	if ( 0 < $difference->days ) {
-		$timestamp .= sprintf( translate_nooped_plural( _n_noop( '%s day', '%s days' ), $difference->days ), $difference->days ) . ' ago.';
+	if ( 0 < $difference->y ) {
+		/**
+		 * If we've passed one year, let's show the full
+		 * date.
+		*/
+		 $timestamp = get_the_date( 'F j, Y' );
+	} else if ( 12 >= $difference->m && 1 <= $difference->m ) {
+		/**
+		 * If we've made it here, we know that we have not
+		 * yet passed one year, but have made it passed one
+		 * month. As such, let's remove the year from the 
+		 * output, but keep the date style format.
+		*/
+		$timestamp .= get_the_date( 'F j' );
+	} else if ( 0 < $difference->d ) {
+		/**
+		 * If we've made it here, we know that we have not
+		 * yet passed one month, but have made it passed one
+		 * day. As such, let's show just the number of days
+		 * that have passed.
+		*/
+		$timestamp .= sprintf( translate_nooped_plural( _n_noop( '%s Day Ago', '%s Days Ago' ), $difference->days ), $difference->days );
+	} else if ( 0 < $difference->h ) {
+		/**
+		 * If we've made it here, we know that we have not
+		 * yet passed one day, but have made it passed one
+		 * hour. As such, let's show just the number of hours
+		 * that have passed.
+		*/
+		$timestamp .= sprintf( translate_nooped_plural( _n_noop( '%s Hour Ago', '%s Hours Ago', 'listed' ), $difference->h, 'listed' ), $difference->h );
+	} else if ( 0 < $difference->i ) {
+		/**
+		 * If we've made it here, we know that we have not
+		 * yet passed one hour, but have made it passed one
+		 * minute. As such, let's show just the number of
+		 * minutes that have passed.
+		*/
+		$timestamp .= sprintf( translate_nooped_plural( _n_noop( '%s Minute Ago', '%s Minutes Ago', 'listed' ), $difference->i, 'listed' ), $difference->i );
 	} else {
-		if ( 0 == $difference->i && 0 == $difference->h ) {
-			$timestamp = __( 'Less than a minute ago.', 'listed' );
-		} else {
-			if ( 0 < $difference->h ) {
-				$timestamp .= sprintf( translate_nooped_plural( _n_noop( '%s hour ago', '%s hours ago', 'listed' ), $difference->h, 'listed' ), $difference->h );
-			} else {
-				$timestamp .= sprintf( translate_nooped_plural( _n_noop( '%s minute ago', '%s minutes ago', 'listed' ), $difference->i, 'listed' ), $difference->i );
-			}
-		}
+		/**
+		 * If we've made it here, that this post is fresh
+		 * off the press. Let's show how fresh it is.
+		*/
+		$timestamp = __( 'Just now', 'listed' );
 	}
 
 	return $timestamp;
