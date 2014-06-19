eca93ddf2a284176466ba604f9fe205fc03a9a65
Justin Kopepasah <justin@kopepasah.com>
2014-06-19 20:40:41 +0900

    Add zip compression to Grunt.

--- gruntfile.js
+++ gruntfile.js
@@ -24,16 +24,53 @@ module.exports = function(grunt) {
 					livereload: true,
 				}
 			}
-		}
+		},
+		compress: {
+			main: {
+				options: {
+					mode: 'zip',
+					archive: function() {
+						return 'releases/' + name + '.zip';
+					}
+				},
+				files: [
+					{
+						expand: true,
+						src: [
+							'**',
+							'!.gitignore',
+							'!.DS_Store',
+							'!style.less',
+							'!package.json',
+							'!gruntfile.js',
+							'!.git/**',
+							'!less/**',
+							'!node_modules/**',
+							'!releases/**'
+						]
+					}
+				]
+		    },
+		},
 	});
 
 	// Load tasks
 	grunt.loadNpmTasks( 'grunt-contrib-less' );
-	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-compress' );
+
+	grunt.registerTask( 'zip', 'Make a zip file for the project.', function( name ){
+		if ( name ) {
+		    grunt.log.writeln( 'Zipping up the project with the name "' + name + '".');
+			global.name = name;
+		    grunt.task.run( 'compress' );
+		} else {
+			grunt.fail.fatal( 'No project name provided for the zip. Please run "grunt zip:name".' );
+		}
+	});
 
 	// Register tasks
 	grunt.registerTask( 'default', [
-		'less',
-		'watch'
+		'less'
 	]);
 };
