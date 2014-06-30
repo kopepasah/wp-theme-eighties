0ad1fde5b4dd8e920b5022e0a2e958b8745b7fa3
Justin Kopepasah <justin@kopepasah.com>
2014-06-30 14:40:12 +0900

    Remove posted on template tag.
    We are not using this template tag in the theme; as such, there is not need for it to exist.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -63,37 +63,6 @@ function eighties_post_nav() {
 }
 endif;
 
-if ( ! function_exists( 'eighties_posted_on' ) ) :
-/**
- * Prints HTML with meta information for the current post-date/time and author.
- * REMOVE This is replaced by time difference.
- */
-function eighties_posted_on() {
-	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
-	}
-
-	$time_string = sprintf( $time_string,
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_attr( get_the_modified_date( 'c' ) ),
-		esc_html( get_the_modified_date() )
-	);
-
-	printf( __( '<span class="posted-on">Posted on %1$s</span><span class="byline"> by %2$s</span>', 'eighties' ),
-		sprintf( '<a href="%1$s" rel="bookmark">%2$s</a>',
-			esc_url( esc_url( get_permalink() ) ),
-			$time_string
-		),
-		sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s">%2$s</a></span>',
-			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-			esc_html( get_the_author() )
-		)
-	);
-}
-endif;
-
 if ( ! function_exists( 'eighties_comment' ) ) :
 /**
  * Template for comments and pingbacks.
