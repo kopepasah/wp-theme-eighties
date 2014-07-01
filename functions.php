7499703593dca7b431df1e44ea7975a1cd0ed4d0
Justin Kopepasah <justin@kopepasah.com>
2014-07-02 03:24:22 +0900

    Added social navigation menu.
    Resolves #5.

--- functions.php
+++ functions.php
@@ -66,6 +66,7 @@ function eighties_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => __( 'Primary Menu', 'eighties' ),
+		'social'  => __( 'Social Menu', 'eighties' )
 	) );
 
 	// Enable support for Post Formats.
